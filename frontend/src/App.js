f9fd1819bc8ebde359ef1739296e1c6901561e96
mannymnz <manuelmunoz5404@gmail.com>
2022-11-17 22:00:43 -0800

    added pages stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,10 @@
-import './App.css';
-import Navbar from "./Components/Navbar";
-import Map from "./Components/Map";
-
+import './css/App.css';
+import HomePage from './pages/HomePage.js'
 function App() {
  
     return (
       <div className="App">
-        <Navbar/>
-          <div id = "headerDiv"> 
-            <h1 id="mainHeader">welcome to <i>appnamehere</i>.</h1>
-            <p> a website designed to help Berkeley students to find study groups across campus.</p>
-            <div id = "horizantalDiv">
-              <MapContainer height = "300px" width = "300px"/>
-            </div>
-            <button id = "plusButton"> + </button>
-          </div>  
+        <HomePage />
       </div>
     );
 }
