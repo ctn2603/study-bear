7f956705e9918e9bf0128c4c8ed32a517ca93b5a
n40m1m <113409723+n40m1m@users.noreply.github.com>
2022-11-28 22:24:09 -0800

    updated navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,5 @@
 import Container from 'react-bootstrap/Container';
-
-import logo from "../images/books.png";
+import logo from "../images/logo.png";
 import ToggleButton from "./ToggleButton";
 import LoginPopup from "./LoginPopup";
 import '../css/Navbar.css'
@@ -11,22 +10,22 @@ import Button from 'react-bootstrap/Button';
 function Navbar() {
   return (
     <div id="background">
+      <a href="http://localhost:3000/">
       <img    id="logo"
               src={logo}
               width="40"
               alt="logo"
-
-            />
+              a href=""
+            /></a>
       <div id = "buttonContainer">
         <button id ="loginButton"> login </button>
-        <button id ="signupButton"> signup </button>
+        <Button id ="signupButton" href="/signup"> signup </Button>
+
 
       </div>
       
     </div>
   );
 }
-
-
 
 export default Navbar;
